61846c50cf5bc79806277ce22ab9d0792e48666c
singmann <henrik.singmann@psychologie.uni-freiburg.de>
2017-03-13 18:43:07 +0100

    included pars argument

--- R/stan_wiener.R
+++ R/stan_wiener.R
@@ -13,7 +13,8 @@ stan_wiener_within <- function(alpha, tau, beta, delta,
                                pos_intercepts = "all",
                                chains = 4, warmup = 1000, iter = 2000,
                                thin = 1,
-                               control = list(adapt_delta = 0.99, max_treedepth = 15)
+                               control = list(adapt_delta = 0.99, max_treedepth = 15), 
+                               pars = c("alpha", "tau", "beta", "delta", "sigma", "deltahat", "Omega")
                                ) 
   {
   
@@ -22,12 +23,13 @@ stan_wiener_within <- function(alpha, tau, beta, delta,
                                          pos_intercepts = pos_intercepts)
   
   out <- sampling(stanmodels$wiener_hierarch_within, 
-                          data = data_prep,
-                          init=lapply(seq_len(chains), function(x) wiener_within_init(data = data_prep)),
-                          warmup = warmup,
-                          iter = iter,
-                          chains = chains,
-                          thin = thin,
-                          control = control)
+                  data = data_prep,
+                  init=lapply(seq_len(chains), function(x) wiener_within_init(data = data_prep)),
+                  pars = pars,
+                  warmup = warmup,
+                  iter = iter,
+                  chains = chains,
+                  thin = thin,
+                  control = control)
   return(out)
 }
